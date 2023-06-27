e8717b79713ef40d6fa35ecea60278a29b4bc3a1
johnngure <johnwngure@gmail.com>
2023-06-27 12:12:17 +0300

    constantArrays

---
+++ ES6/READme.md
@@ -0,0 +1,3 @@
+This begginer ES6 JS in backend.
+This is for educational purpose only.
+Not To BE TESTED.
---
+++ ES6/arrays.js
@@ -0,0 +1,8 @@
+// Create an Array:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Change an element:
+cars[0] = "Toyota";
+
+// Add an element:
+cars.push("Audi");
